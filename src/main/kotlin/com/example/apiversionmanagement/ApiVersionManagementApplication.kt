27987ae1f0e3943c26b10dd256454b873333c5a8
Ueibin Kim <at365@naver.com>
2024-01-21 21:29:25 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/apiversionmanagement/ApiVersionManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apiversionmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiVersionManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiVersionManagementApplication>(*args)
+}
